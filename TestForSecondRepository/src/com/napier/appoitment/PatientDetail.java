e0aba2646fe14d8f01ef697acf1e7e67d9dc7ac0
lifeforjava <lifeforjava786@gmail.com>
2018-09-12 02:39:47 +0530

    commited

--- TestForSecondRepository/src/com/napier/appoitment/PatientDetail.java
+++ TestForSecondRepository/src/com/napier/appoitment/PatientDetail.java
@@ -3,8 +3,8 @@ package com.napier.appoitment;
 public class PatientDetail {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("this dfo from the pateient details!!");
 	}
 
 }
