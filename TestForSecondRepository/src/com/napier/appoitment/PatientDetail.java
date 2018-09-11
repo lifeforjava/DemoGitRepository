33ef6dfe15454b571d23e4a599b52f3ceabf45ae
lifeforjava <lifeforjava786@gmail.com>
2018-09-12 02:41:32 +0530

    cxommited

--- TestForSecondRepository/src/com/napier/appoitment/PatientDetail.java
+++ TestForSecondRepository/src/com/napier/appoitment/PatientDetail.java
@@ -3,8 +3,7 @@ package com.napier.appoitment;
 public class PatientDetail {
 
 	public static void main(String[] args) {
-		
-		System.out.println("this dfo from the pateient details!!");
+
 	}
 
 }
