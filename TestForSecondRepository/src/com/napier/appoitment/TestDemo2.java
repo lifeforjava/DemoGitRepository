6f45e8e3c39b709ac721d0150b5074682e955549
lifeforjava <lifeforjava786@gmail.com>
2018-10-15 11:55:57 +0530

    commited

---
+++ TestForSecondRepository/src/com/napier/appoitment/TestDemo2.java
@@ -0,0 +1,16 @@
+package com.napier.appoitment;
+
+public class TestDemo2 {
+
+	public static void main(String[] args) {
+		int x = 100;
+		double y = 100.1;
+		// the = operator assigns a value to variables.
+		// boolean b = (x = y);// compilation error
+		// The == operator compares values to produce a boolean,
+		boolean b = (x == y);
+		System.out.println(b);
+
+	}
+
+}
